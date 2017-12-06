855abd7026f0d7c7aa2f01c5ba1ec15ae92a8785
meg768 <meg768@users.noreply.github.com>
2017-12-06 21:35:58 +0100

    -

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports = function(homebridge) {
-    homebridge.registerPlatform('homebridge-telldus-tellstick-duo', 'Telldus Tellstick Duo', require('./telldus-platform.js'));
+    homebridge.registerPlatform('homebridge-telldus-tellstick-duo', 'Telldus Tellstick Duo', require('./src/telldus-platform.js'));
 };
