2b2bb3252ce0fc22d6d5dbb0dd36647e36e57028
meg768 <meg768@users.noreply.github.com>
2017-12-19 14:42:13 +0100

    -

--- src/switch.js
+++ src/switch.js
@@ -119,6 +119,7 @@ module.exports = class TelldusSwitch extends TelldusAccessory {
         this.platform.notify(this.config.notifyOn);
 
         telldus.turnOnSync(this.device.id);
+        telldus.turnOnSync(this.device.id);
 
         this.timer.setTimer(500, () => {
             telldus.turnOnSync(this.device.id);
@@ -137,6 +138,7 @@ module.exports = class TelldusSwitch extends TelldusAccessory {
         this.platform.alert(this.config.alertOff);
         this.platform.notify(this.config.notifyOff);
 
+        telldus.turnOffSync(this.device.id);
         telldus.turnOffSync(this.device.id);
 
         this.timer.setTimer(500, () => {
