bec6a00f928a38ad74a5f72fce94a0ceb9f0030a
meg768 <meg768@users.noreply.github.com>
2017-12-19 10:39:15 +0100

    -

--- src/notification-switch.js
+++ src/notification-switch.js
@@ -12,22 +12,9 @@ module.exports = class NotificationSwitch extends Switch {
     }
 
     setState(state) {
-
+        this.platform.notifications = true;
         super.setState(state);
-
-        var alert = sprintf('%s %s', this.displayName, state ? 'ON' : 'OFF');
-
-        if (state) {
-            if (this.config.on)
-                alert = this.config.on;
-        }
-        else {
-            if (this.config.off)
-                alert = this.config.off;
-        }
-
         this.platform.notifications = state;
-        this.platform.alert(alert);
     }
 
 };
