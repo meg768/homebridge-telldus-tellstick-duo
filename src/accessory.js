56d1c6c12e6d7fee1a0024912fbd761e218d6586
meg768 <meg768@users.noreply.github.com>
2019-04-14 20:33:59 +0200

    -

--- src/accessory.js
+++ src/accessory.js
@@ -70,7 +70,7 @@ module.exports = class Accessory extends Events {
     }
 
     identify(callback) {
-        this.log('Identify called for accessory', this.device.name);
+        this.log('Identify called for accessory', this.name);
         callback();
     }
 
