7ef36448a5dd4d4d7657e349b35252b4c2dceb29
meg768 <meg768@users.noreply.github.com>
2019-12-02 00:29:48 +0100

    -

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
 module.exports = function(homebridge) {
-    console.log('Loading platform...');
+    console.log('Loading platform ...');
     homebridge.registerPlatform('homebridge-telldus-tellstick-duo', 'Telldus Tellstick Duo', require('./src/platform.js'));
 };
